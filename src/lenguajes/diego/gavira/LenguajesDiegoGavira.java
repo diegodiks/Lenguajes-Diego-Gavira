4d6e70c7a7067f991ccac5958d3dcba22a84999f
T-107 <T-107@PC251>
2016-05-16 19:26:06 -0500

    Hola

---
+++ src/lenguajes/diego/gavira/LenguajesDiegoGavira.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lenguajes.diego.gavira;
+
+/**
+ *
+ * @author T-107
+ */
+public class LenguajesDiegoGavira {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Mi primer programa de Lenguajes");
+        // TODO code application logic here
+    }
+    
+}
